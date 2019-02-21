2e711049b046a8f95c86c7064d466591cae75f00
new-PC\new <omer2wolf@gmail.com>
2019-02-21 20:09:21 +0200

    Third commit

--- src/DB/DaoImpl/CropDaoImpl.java
+++ src/DB/DaoImpl/CropDaoImpl.java
@@ -7,7 +7,7 @@ import DB.Util.ConnectionConfiguration;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-// Hello
+// Hello World
 public class CropDaoImpl implements CropDao {
     @Override
     public void insert(Crop crop) {
